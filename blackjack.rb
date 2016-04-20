98dea66f0696a0e1341191a842be15e98c8a0c0c
Ben Stacey <stakkadagadge@hotmail.com>
2016-04-20 20:08:57 +0100

    Refactoring & improving game

--- blackjack.rb
+++ blackjack.rb
@@ -1,5 +1,4 @@
-module Hand
-
+module Convertable
   def find_player_raw_value(player_hand)
     values = player_hand.map { |value| value[0] }
     sum = 0
@@ -23,13 +22,33 @@ module Hand
   end
 end
 
-class Participant
-  include Hand
-  attr_accessor :cards, :round_finished
+class Hand
+  attr_accessor :cards
+  def initialize
+    @cards = nil
+  end
 
+  def show_cards(hand)
+    cards = hand
+    extract_values(hand)
+  end
+
+  def extract_values(card)
+    string = ""
+    card.each do |character|
+      string += "[#{character[0]} of #{character[1]}]"
+    end
+    string
+  end
+end
+
+class Participant
+  include Convertable
+  attr_accessor :cards, :round_finished, :hand
   def initialize
     @cards = []
     @round_finished = false
+    @hand = Hand.new
   end
 
   def prompt(msg)
@@ -73,18 +92,18 @@ end
 
 class Deck
   attr_accessor :cards
-  DECK = [['2', 'H'], ['3', 'H'], ['4', 'H'], ['5', 'H'], ['6', 'H'], ['7', 'H'], ['8', 'H'],
-          ['9', 'H'], ['10', 'H'], ['J', 'H'], ['Q', 'H'], ['K', 'H'], ['A', 'H'],
-          ['2', 'C'], ['3', 'C'], ['4', 'C'], ['5', 'C'], ['6', 'C'], ['7', 'C'], ['8', 'C'],
-          ['9', 'C'], ['10', 'C'], ['J', 'C'], ['Q', 'C'], ['K', 'C'], ['A', 'C'],
-          ['2', 'S'], ['3', 'S'], ['4', 'S'], ['5', 'S'], ['6', 'S'], ['7', 'S'], ['8', 'S'],
-          ['9', 'S'], ['10', 'S'], ['J', 'S'], ['Q', 'S'], ['K', 'S'], ['A', 'S'],
-          ['2', 'D'], ['3', 'D'], ['4', 'D'], ['5', 'D'], ['6', 'D'], ['7', 'D'], ['8', 'D'],
-          ['9', 'D'], ['10', 'D'], ['J', 'D'], ['Q', 'D'], ['K', 'D'], ['A', 'D']]
+  SUITS = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+  VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
   def initialize
     @cards = []
-    @cards = DECK
+    SUITS.each do |suit|
+      VALUES.each do |face|
+        arr = []
+        arr.push(face, suit)
+        cards << arr
+      end
+    end
   end
 
   def shuffle_cards!
@@ -96,6 +115,7 @@ end
 
 class Game
   attr_accessor :deck, :player, :dealer, :player_sticks
+  DEALER_DRAW_LIMIT = 17
   def initialize
     @deck = Deck.new
     @player = Player.new
@@ -120,8 +140,25 @@ class Game
     prompt "Welcome!"
   end
 
-  def display_hands
-    prompt "You hold #{player.cards}, with a value of [#{player.find_player_raw_value(player.cards)}] dealer shows #{dealer.cards.first} and a face down card"
+  def display_player_cards
+    player.hand.show_cards(player.cards)
+  end
+
+  def display_player_value
+    player.find_player_raw_value(player.cards)
+  end
+
+  def display_dealer_cards
+    dealer.display_entire_hand
+  end
+
+  def display_dealer_value
+    dealer.find_player_raw_value(dealer.cards)
+  end
+
+  def display_initial_hands
+    prompt "You hold: #{display_player_cards} with a value of [#{display_player_value}]"
+    prompt "Dealer shows [#{dealer.cards[0][0]} of #{dealer.cards[0][1]}] and a face down card"
   end
 
   def display_dealer_draws_message
@@ -130,7 +167,7 @@ class Game
   end
 
   def display_player_bust_message
-    prompt "You now hold #{player.cards}, with a total of [#{player.find_player_raw_value(player.cards)}]"
+    prompt "You now hold #{display_player_cards}, with a total of [#{display_player_value}]"
   end
 
   def display_hand_results
@@ -139,8 +176,8 @@ class Game
     elsif player.busted?(player.cards)
       prompt "You're bust!"
     else
-      prompt "Your total is #{player.find_player_raw_value(player.cards)}"
-      prompt "Dealer's total is #{dealer.find_player_raw_value(dealer.cards)}"
+      prompt "Your total is #{display_player_value}"
+      prompt "Dealer's total is #{display_dealer_value}"
     end
   end
 
@@ -182,19 +219,16 @@ class Game
   end
 
   def dealer_draws?
-    dealer.find_player_raw_value(dealer.cards) < 15
+    dealer.find_player_raw_value(dealer.cards) < DEALER_DRAW_LIMIT
   end
 
   def set_player_finished
     player.round_finished = true
   end
 
-  def start
-    display_welcome_message
-    deck.shuffle_cards!
-    deal_initial_cards
+  def player_action_loop
     loop do
-      display_hands
+      display_initial_hands
       ask_player_for_response
       break if player_sticks?
       player_draws
@@ -204,18 +238,32 @@ class Game
         break
       end
     end
-    loop do
-      break if player.round_finished
-      dealer.show_dealer_hidden_hand
-      break
-    end
+  end
+
+  def dealer_action_loop
     loop do
       break if player.round_finished
       break unless dealer_draws?
       display_dealer_draws_message
       dealer.draw_card(dealer.cards, deck.cards)
-      dealer.display_entire_hand
+      display_dealer_cards
     end
+  end
+
+  def continue_or_show_dealer_hand
+    loop do
+      break if player.round_finished
+      dealer.show_dealer_hidden_hand
+      break
+    end
+  end
+
+  def start
+    display_welcome_message
+    deck.shuffle_cards!
+    deal_initial_cards
+    player_action_loop
+    dealer_action_loop
     display_hand_results
     display_game_result
   end
